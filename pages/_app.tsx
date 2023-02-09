42eac61fd65579de046721941748b68334d4fb11
Trương Minh Thiện <truongminh@MacBook-Pro-cua-Truong.local>
2023-02-09 21:10:48 +0700

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,35 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+// pages/_app.js
+import localFont from '@next/font/local'
+
+// Font files can be colocated inside of `pages`
+const SFFont = localFont({
+  src: [
+    {
+      path: './SFProText-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './SFProText-Semibold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './SFProText-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-sf',
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${SFFont.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
